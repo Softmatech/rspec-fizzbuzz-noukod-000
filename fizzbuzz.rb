99fd87c3914cb6e1ad65d26befdb63ba29f54dfc
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-09 23:46:34 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(int)
+<<<<<<< HEAD
 
   if int % 3 == 0 && int % 5 == 0
     "FizzBuzz"
@@ -15,3 +16,15 @@ def fizzbuzz(int)
   end
   end
 end
+=======
+  if int % 3 == 0 && int % 5 == 0 
+    "FizzBuzz"
+  else if int % 3 == 0 # if the number int is divisible by 3
+    "Fizz" # Go fizz
+   else if int % 5 == 0 # if the number int is divisible by 5
+    "Buzz" # Go fizz
+  end
+end
+end
+end
+>>>>>>> 7529eea585a06ecb5a0ccbae522b486cedd5a3d7
